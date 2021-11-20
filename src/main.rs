0a3fd09a0e7c1421e32141b6d6db10d828e538b5
Seonghyun Park <shp4rk@gmail.com>
2021-11-20 22:15:31 +0900

    Can replay the two earliest commits

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::{
     path::PathBuf,
 };
 
-use git2::{Commit, ObjectType, Oid, Repository, TreeEntry};
+use git2::{Commit, Index, ObjectType, Oid, Repository, Tree, TreeEntry, TreeWalkMode, TreeWalkResult};
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
@@ -24,7 +24,7 @@ struct TargetRepository<'a> {
     root: String,
     _name: String,
     _email: String,
-    _path_to_oid: BTreeMap<String, Oid>,
+    path_to_oid: BTreeMap<PathBuf, Oid>,
 }
 
 impl<'a> TargetRepository<'a> {
@@ -36,14 +36,31 @@ impl<'a> TargetRepository<'a> {
             root: String::from(path),
             _name: String::from(name),
             _email: String::from(email),
-            _path_to_oid: Default::default(),
+            path_to_oid: Default::default(),
         })
     }
 
+    fn handle_tree(
+        &mut self,
+        tree: &Tree,
+    ) -> anyhow::Result<()> {
+        tree.walk(TreeWalkMode::PostOrder, |_, entry| {
+            // how to get a full path name?
+            // We need to know the directory structure, but `.name()`
+            // only gives us the file name w/o its parent directories.
+            println!("{}", entry.name().unwrap());
+            TreeWalkResult::Ok
+        })?;
+
+        Ok(())
+    }
+
+    /// Recursively copy blob contents.
     fn handle_tree_entry(
         &mut self,
         tent: TreeEntry,
         pathbuf: &PathBuf,
+        index: &Index,
     ) -> anyhow::Result<()> {
         let name = tent.name().unwrap();
         let obj = tent.to_object(&self.source)?;
@@ -52,16 +69,40 @@ impl<'a> TargetRepository<'a> {
 
         match obj.kind() {
             Some(ObjectType::Blob) => {
-                std::fs::write(
-                    pathbuf,
-                    obj.as_blob().unwrap().content())?;
+                // Avoid copying multiple times by memoizing
+                // the previous Oid that is copied before.
+                let entry = self.path_to_oid.get(&pathbuf);
+
+                if entry.is_none() || *entry.unwrap() != obj.id() {
+                    // XXX: How to force udpate with `write`?
+                    let mut root = PathBuf::from(&self.root);
+                    root.push(&pathbuf);
+                    println!("{:?}", &root);
+                    std::fs::write(
+                        &root,
+                        obj.as_blob().unwrap().content())?;
+
+                    // TODO: Update index
+                    let index = &mut self.inner.index()?;
+                    index.add_path(&pathbuf)?;
+
+                    self.path_to_oid.insert(pathbuf, obj.id());
+                }
             },
             Some(ObjectType::Tree) => {
                 let tree = obj.as_tree().unwrap();
-                std::fs::create_dir(&pathbuf)?;
-                for child in tree.iter() {
-                    self.handle_tree_entry(child, &pathbuf)?;
+                let mut root = PathBuf::from(&self.root);
+                root.push(&pathbuf);
+                if !root.exists() {
+                    std::fs::create_dir(&root)?; // Ignore if already exists.
                 }
+                for child in tree.iter() {
+                    self.handle_tree_entry(child, &pathbuf, &index)?;
+                }
+                // TODO: Update index?
+                // TODO: What if we ignore the directories?
+                // let index = &mut self.inner.index()?;
+                // index.add_path(&pathbuf)?;
             },
             _ => unimplemented!(),
         }
@@ -69,17 +110,49 @@ impl<'a> TargetRepository<'a> {
         Ok(())
     }
 
-    fn handle_commit(&mut self, commit: Commit) -> anyhow::Result<()> {
+    /// Handle a commit from the source repository.
+    ///
+    /// 1. Copies blobs (that are reachable from the tree) from the
+    ///    source repository to the destination repository.
+    /// 2. Update index in the destination repositories with updated
+    ///    blobs.
+    /// 3. Commit in the destination repository.
+    ///
+    /// Returns a Oid so that we can create a reference (e.g., main).
+    fn handle_commit(
+        &mut self,
+        commit: &Commit,
+        parents: Option<&[Oid]>,
+    ) -> anyhow::Result<Oid> {
         let tree = commit.tree()?;
-        let _index = self.inner.index()?;
+        let pathbuf = PathBuf::new();
+        let index = self.inner.index()?;
 
-        // Let's ingnore directories for now.
-        let pathbuf = PathBuf::from(&self.root);
         for tent in tree.iter() {
-            self.handle_tree_entry(tent, &pathbuf)?;
+            self.handle_tree_entry(tent, &pathbuf, &index)?;
         }
 
-        Ok(())
+        let sig = self.source.signature()?;
+        let index = &mut self.inner.index()?;
+        let tree_id = index.write_tree()?;
+        let tree = self.inner.find_tree(tree_id)?;
+        let parents = parents
+            .and_then(|oids| {
+                Some(oids.iter()
+                    .map(|oid| {
+                        self.inner.find_commit(*oid).unwrap()
+                    })
+                    .collect::<Vec<_>>())
+            })
+            .unwrap_or(vec![]);
+        let mut hack = vec![]; // Maybe use a macro?
+        for i in 0..parents.len() {
+            hack.push(&parents[i]);
+        }
+        Ok(self.inner.commit(Some("HEAD"),
+                             &sig, &sig,
+                             commit.message().unwrap(),
+                             &tree, &hack[..])?)
     }
 }
 
@@ -118,6 +191,7 @@ fn main() -> anyhow::Result<()> {
     loop {
         let tmp = &commit;
         orig_commits.push_back(tmp.clone());
+        // Current implementation can only handle *linear* history at best.
         match commit.parent(0 /* FIXME? */) {
             Ok(parent) => {
                 commit = parent;
@@ -134,12 +208,21 @@ fn main() -> anyhow::Result<()> {
         &args.arg_email,
     )?;
 
-    // XXX: This will copy *blobs* (directories are now ignored) that
-    // are added by the initial commit of the soruce repository to
-    // the target repository directories.
+    // XXX: This will copy *blobs* that are added by the initial
+    // commit of the soruce repository to the target repository
+    // directories (Note this respects the directory architecture
+    // as well).
     if let Some(initial_commit) = orig_commits.pop_back() {
-        target.handle_commit(initial_commit)?;
+        let target_initial_commit_oid = target.handle_commit(&initial_commit, None)?;
+        if let Some(second_oldest_commit) = orig_commits.pop_back() {
+            let target_initial_commit = Oid::zero();
+            target.handle_commit(
+                &second_oldest_commit,
+                Some(&[target_initial_commit_oid]),
+            )?;
+        }
     }
+
 
     Ok(())
 }
